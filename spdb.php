686ab21fc37971e5413a79661acbc29147077290
Krzysiek <krzysztof.falcman@wp.pl>
2017-01-19 14:28:41 +0100

    Pseudomerge

--- spdb.php
+++ spdb.php
@@ -1,47 +1,40 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <title>LineString Arrows</title>
-	<script src="https://code.jquery.com/jquery-1.11.2.min.js"></script>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="https://openlayers.org/en/v3.20.1/css/ol.css" type="text/css">
-	
-    <!-- The line below is only needed for old environments like Internet Explorer and Android 4.x -->
-    <script src="https://cdn.polyfill.io/v2/polyfill.min.js?features=requestAnimationFrame,Element.prototype.classList,URL"></script>
-    <script src="https://openlayers.org/en/v3.20.1/build/ol.js"></script>
-<<<<<<< e8a8067f15be70cc04d51c00e358128353e47ea3
-	
-	<style media="screen" type="text/css">
-	  .popover-content {
-		width: 280px;
-	  }
-	</style>
-  </head>
-  <body>
-    <div id="map" class="map"><div id="popup"></div></div>
-  </body>
-=======
-</head>
-<body onload="init()">
-    <div style="margin: 20px;">
-        <label for="lineSelect">Linia:</label>
-        <select id="lineSelect" onchange="onLineSelected(this.value)">
-            <option value="-1">Wybierz linię</option>
-        </select>
+    <head>
+        <title>SPDB Projekt</title>
 
-        <label for="variantSelect" style="margin-left: 20px;">Wariant:</label>
-        <select id="variantSelect" onchange="onVariantSelected(this.value)">
-            <option value="-1">Wybierz wariant</option>
-        </select>
-    </div>
-  <div id="map" class="map"></div>
+        <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+        <link rel="stylesheet" href="https://openlayers.org/en/v3.20.1/css/ol.css" type="text/css">
 
->>>>>>> Dodanie wyboru linii autobusowej
-  <script src="js/main.js"></script>
+    	<script src="js/jquery-3.1.1.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+
+        <!-- The line below is only needed for old environments like Internet Explorer and Android 4.x -->
+        <script src="https://cdn.polyfill.io/v2/polyfill.min.js?features=requestAnimationFrame,Element.prototype.classList,URL"></script>
+        <script src="https://openlayers.org/en/v3.20.1/build/ol.js"></script>
+
+        <style media="screen" type="text/css">
+            .popover-content {
+                width: 280px;
+            }
+        </style>
+    </head>
+    <body onload="init()">
+        <div style="margin: 20px;">
+            <label for="lineSelect">Linia:</label>
+            <select id="lineSelect" onchange="onLineSelected(this.value)">
+                <option value="-1">Wybierz linię</option>
+            </select>
+
+            <label for="variantSelect" style="margin-left: 20px;">Wariant:</label>
+            <select id="variantSelect" onchange="onVariantSelected(this.value)">
+                <option value="-1">Wybierz wariant</option>
+            </select>
+        </div>
+        <div id="map" class="map"><div id="popup"></div></div>
+    </body>
+    <script src="js/main.js"></script>
 </html>
-<<<<<<< e8a8067f15be70cc04d51c00e358128353e47ea3
-=======
 
 <script>
     function init() {
@@ -86,4 +79,3 @@
         }
     }
 </script>
->>>>>>> Dodanie wyboru linii autobusowej
